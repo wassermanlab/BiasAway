71b97053f929f07221dae0234c116c80d7c07972
Anthony Mathelier <anthony.mathelier@gmail.com>
2013-11-22 19:50:52 -0800

    Light cleaning of the code respecting PEP8. More need to be done.

--- BiasAway.py
+++ BiasAway.py
@@ -1,175 +1,206 @@
 #!/usr/bin/python
 #*-* coding: utf-8 *-*
 
-################################################################################
+###############################################################################
 # BiasAway with the possibility of using very different ways of
 # generating backgrounds lying into two categories:
 # - Creation of new random sequences:
 #   - di-nucleotide shuffling using the foreground sequences
-#   - di-nucleotide shuffling within a sliding window using foreground sequences
-# - Extraction of sequences from a set of possible background sequences:
+#   - di-nucleotide shuffling within a sliding window using foreground
+#   sequences
+#- Extraction of sequences from a set of possible background sequences:
 #   - respecting the %GC distribution of the foreground (using %GC bins)
 #   - respecting the %GC distribution as in the previous item and also
-#   respecting the %GC composition within a sliding window for %GC bin
-################################################################################
+#     respecting the %GC composition within a sliding window for %GC bin
+###############################################################################
 
-import sys, argparse
+import argparse
 import dinuc_shuffling_generator as dinuc_shuff
 import dinuc_window_shuffling_generator as dinuc_win_shuff
 import GC_compo_matching as GC_compo
 import GC_window_compo_matching as GC_window_compo
-from utils import *
+from utils import get_seqs
 
 
-def shuffling_generator(args):
-  seqs, fg_gc_list, fg_lengths = get_seqs(args.fg_file)
-  bg_gc_list, bg_lengths = dinuc_shuff.generate_sequences(seqs, args.nfold)
-  
-
-def shuffling_window_generator(args):
-  seqs, fg_gc_list, fg_lengths = get_seqs(args.fg_file)
-  bg_gc_list, bg_lengths = dinuc_win_shuff.generate_sequences(seqs, args.winlen, args.step, args.nfold)
-  
-
-def gc_compo_generator(args):
-  if args.len_opt:
-    gc_compo_len_generator(args)
-  else:
-    gc_compo_generator_no_len(args)
+def shuffling_generator(argu):
+    seqs, __, __ = get_seqs(argu.fg_file)
+    __, __ = dinuc_shuff.generate_sequences(seqs, argu.nfold)
 
 
-def gc_compo_generator_no_len(args):
-  fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_GC_bins(args.fg_file)
-  bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_GC_bins(args.bg_file)
-  match_gc_list, match_lengths = GC_compo.generate_sequences(fg_gc_bins, bg_gc_bins, args.nfold)
+def shuffling_window_generator(argu):
+    seqs, __, __ = get_seqs(argu.fg_file)
+    __, __ = dinuc_win_shuff.generate_sequences(seqs, argu.winlen,
+                                                argu.step,
+                                                argu.nfold)
 
 
-def gc_compo_len_generator(args):
-  fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_len_GC_bins(args.fg_file)
-  bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_len_GC_bins(args.bg_file)
-  match_gc_list, match_lengths = GC_compo.generate_len_sequences(fg_gc_bins,
-      bg_gc_bins, args.nfold)
+def gc_compo_generator(argu):
+    if argu.len_opt:
+        gc_compo_len_generator(argu)
+    else:
+        gc_compo_generator_no_len(argu)
 
 
-def gc_compo_window_generator(args):
-  if args.len_opt:
-    gc_compo_len_window_generator(args)
-  else:
-    gc_compo_window_generator_no_len(args)
+def gc_compo_generator_no_len(argu):
+    __, fg_gc_bins, __ = GC_compo.fg_GC_bins(argu.fg_file)
+    __, bg_gc_bins, __ = GC_compo.bg_GC_bins(argu.bg_file)
+    __, __ = GC_compo.generate_sequences(fg_gc_bins, bg_gc_bins,
+                                         argu.nfold)
 
 
-def gc_compo_len_window_generator(args):
-  fg_gc_list, fg_gc_bins, fg_lengths = GC_window_compo.fg_len_GC_bins(args.fg_file, args.winlen,
-      args.step)
-  bg_gc_list, bg_gc_bins, bg_lengths = GC_window_compo.bg_len_GC_bins(args.bg_file)
-  match_gc_list, match_lengths = GC_window_compo.generate_len_sequences(fg_gc_bins, bg_gc_bins,
-      args.deviation, args.winlen, args.step, args.nfold)
+def gc_compo_len_generator(argu):
+    __, fg_gc_bins, __ = GC_compo.fg_len_GC_bins(argu.fg_file)
+    __, bg_gc_bins, __ = GC_compo.bg_len_GC_bins(argu.bg_file)
+    __, __ = GC_compo.generate_len_sequences(fg_gc_bins, bg_gc_bins,
+                                             argu.nfold)
 
 
-def gc_compo_window_generator_no_len(args):
-  fg_gc_list, fg_gc_bins, fg_lengths = GC_window_compo.fg_GC_bins(args.fg_file, args.winlen,
-      args.step)
-  bg_gc_list, bg_gc_bins, bg_lengths = GC_window_compo.bg_GC_bins(args.bg_file)
-  match_gc_list, match_lengths = GC_window_compo.generate_sequences(fg_gc_bins, bg_gc_bins, args.deviation,
-      args.winlen, args.step, args.nfold)
+def gc_compo_window_generator(argu):
+    if argu.len_opt:
+        gc_compo_len_window_generator(argu)
+    else:
+        gc_compo_window_generator_no_len(argu)
+
+
+def gc_compo_len_window_generator(argu):
+    __, fg_gc_bins, __ = GC_window_compo.fg_len_GC_bins(argu.fg_file,
+                                                        argu.winlen, argu.step)
+    __, bg_gc_bins, __ = GC_window_compo.bg_len_GC_bins(argu.bg_file)
+    __, __ = GC_window_compo.generate_len_sequences(fg_gc_bins, bg_gc_bins,
+                                                    argu.deviation,
+                                                    argu.winlen, argu.step,
+                                                    argu.nfold)
+
+
+def gc_compo_window_generator_no_len(argu):
+    __, fg_gc_bins, __ = GC_window_compo.fg_GC_bins(argu.fg_file, argu.winlen,
+                                                    argu.step)
+    __, bg_gc_bins, __ = GC_window_compo.bg_GC_bins(argu.bg_file)
+    __, __ = GC_window_compo.generate_sequences(fg_gc_bins, bg_gc_bins,
+                                                argu.deviation, argu.winlen,
+                                                argu.step, argu.nfold)
 
 
 def shuffling_arg_parsing(subparsers):
-  parser_d = subparsers.add_parser('d', 
-      help="di-nucleotide shuffling generator")
-  parser_d.add_argument('-f', '--foreground', required=True, type=str,
-      dest="fg_file", action="store", help="Foreground file in fasta format")
-  parser_d.add_argument('-n', '--nfold', required=False, type=int, dest="nfold",
-      action="store", default=1,
-      help="How many background sequences per each foreground sequence will be generated (default: 1)")
-  parser_d.set_defaults(func=shuffling_generator)
+    parser_d = subparsers.add_parser('d',
+                                     help="di-nucleotide shuffling generator")
+    parser_d.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be generated (default: 1)"
+    parser_d.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    parser_d.set_defaults(func=shuffling_generator)
 
 
 def window_shuffling_arg_parsing(subparsers):
-  parser_w = subparsers.add_parser('w',
-      help="di-nucleotide shuffling within a sliding window generator")
-  parser_w.add_argument("-w", "--winlen", required=False, type=int,
-      dest="winlen", action="store", default=200, 
-      help="Window length (default: 200)")
-  parser_w.add_argument("-s", "--step", required=False, type=int,
-      dest="step", action="store", default=1, 
-      help="Sliding step (default: 1)")
-  parser_w.add_argument('-f', '--foreground', required=True, type=str,
-      dest="fg_file", action="store", help="Foreground file in fasta format")
-  parser_w.add_argument('-n', '--nfold', required=False, type=int, dest="nfold",
-      action="store", default=1,
-      help="How many background sequences per each foreground sequence will be generated (default: 1)")
-  parser_w.set_defaults(func=shuffling_window_generator)
+    help_str = "di-nucleotide shuffling within a sliding window generator"
+    parser_w = subparsers.add_parser('w', help=help_str)
+    parser_w.add_argument("-w", "--winlen", required=False, type=int,
+                          dest="winlen", action="store", default=200,
+                          help="Window length (default: 200)")
+    parser_w.add_argument("-s", "--step", required=False, type=int,
+                          dest="step", action="store", default=1,
+                          help="Sliding step (default: 1)")
+    parser_w.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be generated (default: 1)"
+    parser_w.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    parser_w.set_defaults(func=shuffling_window_generator)
 
-  
-def gc_compo_arg_parsing(subparsers):  
-  parser_g = subparsers.add_parser('g',
-      help="%%GC distribution-based background chooser")
-  parser_g.add_argument("-b", "--background", required=True, type=str, dest="bg_file",
-      action="store", help="Background file in fasta format")
-  parser_g.add_argument('-f', '--foreground', required=True, type=str,
-      dest="fg_file", action="store", help="Foreground file in fasta format")
-  parser_g.add_argument('-n', '--nfold', required=False, type=int, dest="nfold",
-      action="store", default=1,
-      help="How many background sequences per each foreground sequence will be choosen (default: 1)")
-  parser_g.add_argument('-l', '--length', required=False, dest="len_opt",
-      action="store_const", const=1, default=0,
-      help="Try to match the length as   closely as possible (not set by default)")
-  parser_g.set_defaults(func=gc_compo_generator)
+
+def gc_compo_arg_parsing(subparsers):
+    help_str = "%%GC distribution-based background chooser"
+    parser_g = subparsers.add_parser('g', help=help_str)
+    parser_g.add_argument("-b", "--background", required=True, type=str,
+                          dest="bg_file", action="store",
+                          help="Background file in fasta format")
+    parser_g.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be choosen (default: 1)"
+    parser_g.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    help_str = "Try to match the length as closely as possible "
+    help_str += "(not set by default)"
+    parser_g.add_argument('-l', '--length', required=False, dest="len_opt",
+                          action="store_const", const=1, default=0,
+                          help=help_str)
+    parser_g.set_defaults(func=gc_compo_generator)
 
 
 def gc_compo_window_arg_parsing(subparsers):
-  parser_c = subparsers.add_parser('c',
-      help="%%GC distribution and %%GC composition within a sliding window background chooser")
-  parser_c.add_argument("-b", "--background", required=True, type=str, dest="bg_file",
-      action="store", help="Background file in fasta format")
-  parser_c.add_argument("-w", "--winlen", required=False, type=int,
-      dest="winlen", action="store", default=200, 
-      help="Window length (default: 200)")
-  parser_c.add_argument("-s", "--step", required=False, type=int,
-      dest="step", action="store", default=1, 
-      help="Sliding step (default: 1)")
-  parser_c.add_argument("-d", "--deviation", required=False, type=float,
-      dest="deviation", action="store", default=2.6,
-      help="Deviation from the mean (default: 2.6 for a threshold of mean + 2.6 * stdev)")
-  parser_c.add_argument('-f', '--foreground', required=True, type=str,
-      dest="fg_file", action="store", help="Foreground file in fasta format")
-  parser_c.add_argument('-n', '--nfold', required=False, type=int, dest="nfold",
-      action="store", default=1,
-      help="How many background sequences per each foreground sequence will be choosen (default: 1)")
-  parser_c.add_argument('-l', '--length', required=False, dest="len_opt",
-      action="store_const", const=1, default=0,
-      help="Try to match the length as   closely as possible (not set by default)")
-  parser_c.set_defaults(func=gc_compo_window_generator)
+    help_str = "%%GC distribution and %%GC composition within a sliding "
+    help_str += "window background chooser"
+    parser_c = subparsers.add_parser('c', help=help_str)
+    parser_c.add_argument("-b", "--background", required=True, type=str,
+                          dest="bg_file", action="store",
+                          help="Background file in fasta format")
+    parser_c.add_argument("-w", "--winlen", required=False, type=int,
+                          dest="winlen", action="store", default=200,
+                          help="Window length (default: 200)")
+    parser_c.add_argument("-s", "--step", required=False, type=int,
+                          dest="step", action="store", default=1,
+                          help="Sliding step (default: 1)")
+    help_str = "Deviation from the mean (default: 2.6 for a "
+    help_str += "threshold of mean + 2.6 * stdev)"
+    parser_c.add_argument("-d", "--deviation", required=False, type=float,
+                          dest="deviation", action="store", default=2.6,
+                          help=help_str)
+    parser_c.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be choosen (default: 1)"
+    parser_c.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    help_str = "Try to match the length as closely as possible "
+    help_str += "(not set by default)"
+    parser_c.add_argument('-l', '--length', required=False, dest="len_opt",
+                          action="store_const", const=1, default=0,
+                          help=help_str)
+    parser_c.set_defaults(func=gc_compo_window_generator)
 
 
 def arg_parsing():
-  descr = '''Background generator with the possibility of using very
-  different ways of generating backgrounds lying into two categories:
-    - Creation of new random sequences (generators):
-      - di-nucleotide shuffling using the foreground sequences
-      - di-nucleotide shuffling within a sliding window using foreground
-        sequences
-    - Extraction of sequences from a set of possible background sequences (choosers):
-      - respecting the %GC distribution of the foreground (using %GC bins)
-      - respecting the %GC distribution as in the previous item and also
-      respecting the %GC composition within a sliding window for %GC bin
-  '''
-  parser = argparse.ArgumentParser(description=descr,
-      formatter_class=argparse.RawDescriptionHelpFormatter)
-  subparsers = parser.add_subparsers(help="Choice of the generator/chooser", 
-      title="Subcommands", description="Valid subcommands")
-  shuffling_arg_parsing(subparsers)
-  window_shuffling_arg_parsing(subparsers)
-  gc_compo_arg_parsing(subparsers)
-  gc_compo_window_arg_parsing(subparsers)
-  args = parser.parse_args()
-  return args
+    descr = '''Background generator with the possibility of using very
+    different ways of generating backgrounds lying into two categories:
+        - Creation of new random sequences (generators):
+            - di-nucleotide shuffling using the foreground sequences
+            - di-nucleotide shuffling within a sliding window using foreground
+                sequences
+        - Extraction of sequences from a set of possible background sequences
+          (choosers):
+            - respecting the %GC distribution of the foreground (using %GC
+              bins)
+            - respecting the %GC distribution as in the previous item and also
+            respecting the %GC composition within a sliding window for %GC bin
+    '''
+    parser = argparse.ArgumentParser(description=descr,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(help="Choice of the generator/chooser",
+                                       title="Subcommands",
+                                       description="Valid subcommands")
+    shuffling_arg_parsing(subparsers)
+    window_shuffling_arg_parsing(subparsers)
+    gc_compo_arg_parsing(subparsers)
+    gc_compo_window_arg_parsing(subparsers)
+    argu = parser.parse_args()
+    return argu
 
 
-################################################################################
-#                               MAIN
-################################################################################
+###############################################################################
+#                                   MAIN
+###############################################################################
 if __name__ == "__main__":
-  args = arg_parsing()
-  args.func(args)
+    arguments = arg_parsing()
+    arguments.func(arguments)
--- utils.py
+++ utils.py
@@ -1,139 +1,156 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
-import tempfile, re, os, sys
+import re
+import sys
 
 
-def GC(seq): 
- """Calculates G+C content, returns the percentage (float between 0 and 100). 
- Copes mixed case sequences, and with the ambiguous nucleotide S (G or C) 
- when counting the G and C content.  The percentage is calculated against 
- the length of the sequence using A,C,G,T,S,W with Ns, e.g.:  
- >>> GC("ACTGN") 
- 50.0 
- Note that this will return zero for an empty sequence. 
- """ 
- try:
-   gc = sum(map(seq.count, ['G','C','g','c','S','s']))
-   l = sum(map(seq.count, ['G', 'C', 'A', 'T', 'S', 'W', 'g', 'c', 'a', 't',
-     's', 'w']))
-   return gc*100/l 
- except ZeroDivisionError: 
-   return 0
+def GC(seq):
+    """
+    Calculate G+C content, return the percentage (float between 0 and 100).
+    Copes mixed case sequences, and with the ambiguous nucleotide S (G or C)
+    when counting the G and C content.  The percentage is calculated against
+    the length of the sequence using A,C,G,T,S,W with Ns, e.g.:
+    >>> GC("ACTGN")
+    50.0 Note that this will return zero for an empty sequence.
+
+    """
+    try:
+        gc = sum(map(seq.count, ['G', 'C', 'g', 'c', 'S', 's']))
+        l = sum(map(seq.count, ['G', 'C', 'A', 'T', 'S', 'W', 'g', 'c', 'a',
+                                't', 's', 'w']))
+        return gc * 100 / l
+    except ZeroDivisionError:
+        return 0
 
 
 def get_seqs(f):
-  seqs = []
-  fg_gc_list = []
-  fg_lengths = []
-  stream = open(f)
-  for record in SeqIO.parse(f, "fasta"):
-    record.seq = record.seq.upper()
-    seqs.append(record)
-    fg_gc_list.append(GC(record.seq))
-    fg_lengths.append(len(record.seq))
-  stream.close()
-  return seqs, fg_gc_list, fg_lengths
+    seqs = []
+    fg_gc_list = []
+    fg_lengths = []
+    stream = open(f)
+    for record in SeqIO.parse(f, "fasta"):
+        record.seq = record.seq.upper()
+        seqs.append(record)
+        fg_gc_list.append(GC(record.seq))
+        fg_lengths.append(len(record.seq))
+    stream.close()
+    return seqs, fg_gc_list, fg_lengths
 
 
 def init_compo(length):
-  dico = []
-  for i in range(1, length):
-    dico.append({})
-    j = i-1
-    dico[j]["AA"] = 0.0
-    dico[j]["AC"] = 0.0
-    dico[j]["AT"] = 0.0
-    dico[j]["AG"] = 0.0
-    dico[j]["CA"] = 0.0
-    dico[j]["CC"] = 0.0
-    dico[j]["CT"] = 0.0
-    dico[j]["CG"] = 0.0
-    dico[j]["GA"] = 0.0
-    dico[j]["GC"] = 0.0
-    dico[j]["GG"] = 0.0
-    dico[j]["GT"] = 0.0
-    dico[j]["TA"] = 0.0
-    dico[j]["TC"] = 0.0
-    dico[j]["TG"] = 0.0
-    dico[j]["TT"] = 0.0
-  return dico
+    dico = []
+    for i in range(1, length):
+        dico.append({})
+        j = i - 1
+        dico[j]["AA"] = 0.0
+        dico[j]["AC"] = 0.0
+        dico[j]["AT"] = 0.0
+        dico[j]["AG"] = 0.0
+        dico[j]["CA"] = 0.0
+        dico[j]["CC"] = 0.0
+        dico[j]["CT"] = 0.0
+        dico[j]["CG"] = 0.0
+        dico[j]["GA"] = 0.0
+        dico[j]["GC"] = 0.0
+        dico[j]["GG"] = 0.0
+        dico[j]["GT"] = 0.0
+        dico[j]["TA"] = 0.0
+        dico[j]["TC"] = 0.0
+        dico[j]["TG"] = 0.0
+        dico[j]["TT"] = 0.0
+    return dico
 
 
 def length(record):
-  return len(record.seq)
+    return len(record.seq)
 
 
 def compute_dinuc_distrib(seqs, b=False):
-  max_length = max(map(length, seqs))
-  compo = init_compo(max_length)
-  for i in range(0, len(seqs)):
-    seq_length = len(seqs[i].seq)
-    for j in range(1, seq_length):
-      if seqs[i].seq[j-1] <> 'N' and seqs[i].seq[j] <> 'N':
-        compo[j-1]["%s"%(seqs[i].seq[(j-1):(j+1)])] += 1.0
+    max_length = max(map(length, seqs))
+    compo = init_compo(max_length)
+    for i in range(0, len(seqs)):
+        seq_length = len(seqs[i].seq)
+        for j in range(1, seq_length):
+            if seqs[i].seq[j - 1] != 'N' and seqs[i].seq[j] != 'N':
+                compo[j - 1]["%s"%(seqs[i].seq[(j - 1):(j + 1)])] += 1.0
 
-  if b: # Dinucleotide distrib over all positions
-    distrib = {}
-    composition = {}
-    for key in compo[0].keys():
-      cpt = 0.0
-      for i in range(1, max_length):
-        cpt += compo[i-1][key]
-      composition[key] = cpt
-    for k in composition.keys():
-      cpt = 4.0 # WARNING: WE DO NOT TAKE ANY 'N' INTO ACCOUNT
-      first = k[0]
-      for key in composition.keys():
-        if key[0] == first:
-          cpt += composition[key]
-      distrib[k] = (composition[k] + 1.0) / cpt
-  else: # Dinucleotide distrib position by position
-    distrib = init_compo(seq_length)
-    for j in range(1, seq_length):
-      for k in compo[j-1].keys():
-        if re.search("N", k):
-          distrib[j-1][k] = 0.0
-        else:
-          cpt = 4.0
-          first = k[0]
-          for key in compo[j-1].keys():
-            if key[0] == first:
-              cpt += compo[j-1][key] 
-          distrib[j-1][k] = (compo[j-1][k] + 1.0) / cpt
-  return distrib
+    if b:  # Dinucleotide distrib over all positions
+        distrib = {}
+        composition = {}
+        for key in compo[0].keys():
+            cpt = 0.0
+            for i in range(1, max_length):
+                cpt += compo[i - 1][key]
+            composition[key] = cpt
+        for k in composition.keys():
+            cpt = 4.0  # WARNING: WE DO NOT TAKE ANY 'N' INTO ACCOUNT
+            first = k[0]
+            for key in composition.keys():
+                if key[0] == first:
+                    cpt += composition[key]
+            distrib[k] = (composition[k] + 1.0) / cpt
+    else:  # Dinucleotide distrib position by position
+        distrib = init_compo(seq_length)
+        for j in range(1, seq_length):
+            for k in compo[j - 1].keys():
+                if re.search("N", k):
+                    distrib[j - 1][k] = 0.0
+                else:
+                    cpt = 4.0
+                    first = k[0]
+                    for key in compo[j - 1].keys():
+                        if key[0] == first:
+                            cpt += compo[j - 1][key]
+                    distrib[j - 1][k] = (compo[j - 1][k] + 1.0) / cpt
+    return distrib
 
 
 def print_dinuc_distrib(dinuc, output):
-  stream = sys.stdout
-  if output:
-    stream = open(output, "w")
-  for j in range(0, len(dinuc)):
-    stream.write("%f, %f, %f, %f, %f, %f, %f, %f, %f,"%(dinuc[j]["AA"],
-      dinuc[j]["AC"], dinuc[j]["AG"], dinuc[j]["AT"], dinuc[j]["AN"],
-      dinuc[j]["CA"], dinuc[j]["CC"], dinuc[j]["CG"], dinuc[j]["CT"]))
-    stream.write(" %f, %f, %f, %f, %f, %f, %f, %f, %f,"%(dinuc[j]["CN"],
-      dinuc[j]["GA"], dinuc[j]["GC"], dinuc[j]["GG"], dinuc[j]["GT"],
-      dinuc[j]["GN"], dinuc[j]["TA"], dinuc[j]["TC"], dinuc[j]["TG"]))
-    stream.write(" %f, %f, %f, %f, %f, %f, %f\n"%(dinuc[j]["TT"],
-      dinuc[j]["TN"], dinuc[j]["NA"], dinuc[j]["NC"], dinuc[j]["NG"],
-      dinuc[j]["NT"], dinuc[j]["NN"]))
-  stream.close()
+    stream = sys.stdout
+    if output:
+        stream = open(output, "w")
+    for j in range(0, len(dinuc)):
+        stream.write("%f, %f, %f, %f, %f, %f, %f, %f, %f,"%(dinuc[j]["AA"],
+                                                            dinuc[j]["AC"],
+                                                            dinuc[j]["AG"],
+                                                            dinuc[j]["AT"],
+                                                            dinuc[j]["AN"],
+                                                            dinuc[j]["CA"],
+                                                            dinuc[j]["CC"],
+                                                            dinuc[j]["CG"],
+                                                            dinuc[j]["CT"]))
+        stream.write(" %f, %f, %f, %f, %f, %f, %f, %f, %f,"%(dinuc[j]["CN"],
+                                                             dinuc[j]["GA"],
+                                                             dinuc[j]["GC"],
+                                                             dinuc[j]["GG"],
+                                                             dinuc[j]["GT"],
+                                                             dinuc[j]["GN"],
+                                                             dinuc[j]["TA"],
+                                                             dinuc[j]["TC"],
+                                                             dinuc[j]["TG"]))
+        stream.write(" %f, %f, %f, %f, %f, %f, %f\n"%(dinuc[j]["TT"],
+                                                      dinuc[j]["TN"],
+                                                      dinuc[j]["NA"],
+                                                      dinuc[j]["NC"],
+                                                      dinuc[j]["NG"],
+                                                      dinuc[j]["NT"],
+                                                      dinuc[j]["NN"]))
+    stream.close()
 
 
 def compute_nt_distrib(seqs):
-  cpt = 4.0
-  distrib = {}
-  for l in "ACGT":
-    distrib[l] = 1.0
-  for seq in seqs:
-    for l in seq:
-      if l <> 'N':
-        distrib[l] += 1.0
-        cpt += 1.0
-  for l in "ACGT":
-    distrib[l] /= cpt
-  return distrib
+    cpt = 4.0
+    distrib = {}
+    for l in "ACGT":
+        distrib[l] = 1.0
+    for seq in seqs:
+        for l in seq:
+            if l != 'N':
+                distrib[l] += 1.0
+                cpt += 1.0
+    for l in "ACGT":
+        distrib[l] /= cpt
+    return distrib
 
 
 def split_seq(seq):
-  return re.split('(N+)', seq)
+    return re.split('(N+)', seq)
