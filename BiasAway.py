97418fcf18085092b3346101569d86548a3cdcdc
Anthony Mathelier <anthony.mathelier@gmail.com>
2014-04-22 11:49:46 -0700

    Addition of the mononucleotide features.

--- BiasAway.py
+++ BiasAway.py
@@ -7,16 +7,22 @@ BiasAway module generating adapted background for motif overrepresentation.
  BiasAway with the possibility of using very different ways of
  generating backgrounds lying into two categories:
  - Creation of new random sequences:
+   - mono-nucleotide shuffling using the foreground sequences
+   - mono-nucleotide shuffling within a sliding window using foreground
+     sequences
    - di-nucleotide shuffling using the foreground sequences
-   - di-nucleotide shuffling within a sliding window using foreground sequences
- - Extraction of sequences from a set of possible background sequences:
+   - di-nucleotide shuffling within a sliding window using foreground
+     sequences
+- Extraction of sequences from a set of possible background sequences:
    - respecting the %GC distribution of the foreground (using %GC bins)
    - respecting the %GC distribution as in the previous item and also
-   respecting the %GC composition within a sliding window for %GC bin
+     respecting the %GC composition within a sliding window for %GC bin
 
 """
 
 import argparse
+import mononuc_shuffling_generator as mononuc_shuff
+import mononuc_window_shuffling_generator as mononuc_win_shuff
 import dinuc_shuffling_generator as dinuc_shuff
 import dinuc_window_shuffling_generator as dinuc_win_shuff
 import GC_compo_matching as GC_compo
@@ -24,40 +30,61 @@ import GC_window_compo_matching as GC_window_compo
 from utils import get_seqs, make_r_gc_plots, make_r_len_plots
 
 
-def shuffling_generator(args):
-    seqs, fg_gc_list, fg_lengths = get_seqs(args.fg_file)
-    bg_gc_list, bg_lengths = dinuc_shuff.generate_sequences(seqs, args.nfold)
+def mononuc_shuffling_generator(argu):
+    seqs, fg_gc_list, fg_lengths = get_seqs(argu.fg_file)
+    bg_gc_list, bg_lengths = mononuc_shuff.generate_sequences(seqs, argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Generated background", "GC", "\nGC plot")
     make_r_len_plots(fg_lengths, bg_lengths, "Foreground",
                      "Generated background", "Len", "\nLength plot")
 
 
-def shuffling_window_generator(args):
-    seqs, fg_gc_list, fg_lengths = get_seqs(args.fg_file)
+def dinuc_shuffling_generator(argu):
+    seqs, fg_gc_list, fg_lengths = get_seqs(argu.fg_file)
+    bg_gc_list, bg_lengths = dinuc_shuff.generate_sequences(seqs, argu.nfold)
+    make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
+                    "Generated background", "GC", "\nGC plot")
+    make_r_len_plots(fg_lengths, bg_lengths, "Foreground",
+                     "Generated background", "Len", "\nLength plot")
+
+
+def mononuc_shuffling_window_generator(argu):
+    seqs, fg_gc_list, fg_lengths = get_seqs(argu.fg_file)
+    bg_gc_list, bg_lengths = mononuc_win_shuff.generate_sequences(seqs,
+                                                                  argu.winlen,
+                                                                  argu.step,
+                                                                  argu.nfold)
+    make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
+                    "Generated background", "GC", "\nGC plot")
+    make_r_len_plots(fg_lengths, bg_lengths, "Foreground",
+                     "Generated background", "Len", "\nLength plot")
+
+
+def dinuc_shuffling_window_generator(argu):
+    seqs, fg_gc_list, fg_lengths = get_seqs(argu.fg_file)
     bg_gc_list, bg_lengths = dinuc_win_shuff.generate_sequences(seqs,
-                                                                args.winlen,
-                                                                args.step,
-                                                                args.nfold)
+                                                                argu.winlen,
+                                                                argu.step,
+                                                                argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Generated background", "GC", "\nGC plot")
     make_r_len_plots(fg_lengths, bg_lengths, "Foreground",
                      "Generated background", "Len", "\nLength plot")
 
 
-def gc_compo_generator(args):
-    if args.len_opt:
-        gc_compo_len_generator(args)
+def gc_compo_generator(argu):
+    if argu.len_opt:
+        gc_compo_len_generator(argu)
     else:
-        gc_compo_generator_no_len(args)
+        gc_compo_generator_no_len(argu)
 
 
-def gc_compo_generator_no_len(args):
-    fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_GC_bins(args.fg_file)
-    bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_GC_bins(args.bg_file)
+def gc_compo_generator_no_len(argu):
+    fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_GC_bins(argu.fg_file)
+    bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_GC_bins(argu.bg_file)
     match_gc_list, match_lengths = GC_compo.generate_sequences(fg_gc_bins,
                                                                bg_gc_bins,
-                                                               args.nfold)
+                                                               argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Background", "GCbefore", "\nGC plot before")
     make_r_gc_plots(fg_gc_list, match_gc_list, "Foreground",
@@ -68,12 +95,12 @@ def gc_compo_generator_no_len(args):
                      "Matching Background", "Lenafter", "Length plot after")
 
 
-def gc_compo_len_generator(args):
-    fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_len_GC_bins(args.fg_file)
-    bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_len_GC_bins(args.bg_file)
+def gc_compo_len_generator(argu):
+    fg_gc_list, fg_gc_bins, fg_lengths = GC_compo.fg_len_GC_bins(argu.fg_file)
+    bg_gc_list, bg_gc_bins, bg_lengths = GC_compo.bg_len_GC_bins(argu.bg_file)
     match_gc_list, match_lengths = GC_compo.generate_len_sequences(fg_gc_bins,
                                                                    bg_gc_bins,
-                                                                   args.nfold)
+                                                                   argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Background", "GCbefore", "\nGC plot before")
     make_r_gc_plots(fg_gc_list, match_gc_list, "Foreground",
@@ -84,21 +111,21 @@ def gc_compo_len_generator(args):
                      "Matching Background", "Lenafter", "Length plot after")
 
 
-def gc_compo_window_generator(args):
-    if args.len_opt:
-        gc_compo_len_window_generator(args)
+def gc_compo_window_generator(argu):
+    if argu.len_opt:
+        gc_compo_len_window_generator(argu)
     else:
-        gc_compo_window_generator_no_len(args)
+        gc_compo_window_generator_no_len(argu)
 
 
-def gc_compo_len_window_generator(args):
+def gc_compo_len_window_generator(argu):
     fg_gc_list, fg_gc_bins, fg_lengths = GC_window_compo.fg_len_GC_bins(
-        args.fg_file, args.winlen, args.step)
+        argu.fg_file, argu.winlen, argu.step)
     bg_gc_list, bg_gc_bins, bg_lengths = GC_window_compo.bg_len_GC_bins(
-        args.bg_file)
+        argu.bg_file)
     match_gc_list, match_lengths = GC_window_compo.generate_len_sequences(
-        fg_gc_bins, bg_gc_bins, args.deviation, args.winlen, args.step,
-        args.nfold)
+        fg_gc_bins, bg_gc_bins, argu.deviation, argu.winlen, argu.step,
+        argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Background", "GCbefore", "\nGC plot before")
     make_r_gc_plots(fg_gc_list, match_gc_list, "Foreground",
@@ -109,14 +136,14 @@ def gc_compo_len_window_generator(args):
                      "Matching Backgronud", "Lenafter", "Length plot after")
 
 
-def gc_compo_window_generator_no_len(args):
+def gc_compo_window_generator_no_len(argu):
     fg_gc_list, fg_gc_bins, fg_lengths = GC_window_compo.fg_GC_bins(
-        args.fg_file, args.winlen, args.step)
+        argu.fg_file, argu.winlen, argu.step)
     bg_gc_list, bg_gc_bins, bg_lengths = GC_window_compo.bg_GC_bins(
-        args.bg_file)
+        argu.bg_file)
     match_gc_list, match_lengths = GC_window_compo.generate_sequences(
-        fg_gc_bins, bg_gc_bins, args.deviation, args.winlen, args.step,
-        args.nfold)
+        fg_gc_bins, bg_gc_bins, argu.deviation, argu.winlen, argu.step,
+        argu.nfold)
     make_r_gc_plots(fg_gc_list, bg_gc_list, "Foreground",
                     "Background", "GCbefore", "\nGC plot before")
     make_r_gc_plots(fg_gc_list, match_gc_list, "Foreground",
@@ -127,7 +154,41 @@ def gc_compo_window_generator_no_len(args):
                      "Matching Backgronud", "Lenafter", "Length plot after")
 
 
-def shuffling_arg_parsing(subparsers):
+def mononuc_shuffling_arg_parsing(subparsers):
+    help_str = "mono-nucleotide shuffling generator"
+    parser_d = subparsers.add_parser('m', help=help_str)
+    parser_d.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be generated (default: 1)"
+    parser_d.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    parser_d.set_defaults(func=mononuc_shuffling_generator)
+
+
+def mononuc_window_shuffling_arg_parsing(subparsers):
+    help_str = "mono-nucleotide shuffling within a sliding window generator"
+    parser_w = subparsers.add_parser('f', help=help_str)
+    parser_w.add_argument("-w", "--winlen", required=False, type=int,
+                          dest="winlen", action="store", default=100,
+                          help="Window length (default: 100)")
+    parser_w.add_argument("-s", "--step", required=False, type=int,
+                          dest="step", action="store", default=1,
+                          help="Sliding step (default: 1)")
+    parser_w.add_argument('-f', '--foreground', required=True, type=str,
+                          dest="fg_file", action="store",
+                          help="Foreground file in fasta format")
+    help_str = "How many background sequences per each foreground sequence "
+    help_str += "will be generated (default: 1)"
+    parser_w.add_argument('-n', '--nfold', required=False, type=int,
+                          dest="nfold", action="store", default=1,
+                          help=help_str)
+    parser_w.set_defaults(func=mononuc_shuffling_window_generator)
+
+
+def dinuc_shuffling_arg_parsing(subparsers):
     parser_d = subparsers.add_parser(
         'd', help="di-nucleotide shuffling generator")
     parser_d.add_argument('-f', '--foreground', required=True, type=str,
@@ -138,10 +199,10 @@ def shuffling_arg_parsing(subparsers):
     parser_d.add_argument('-n', '--nfold', required=False, type=int,
                           dest="nfold", action="store", default=1,
                           help=help_str)
-    parser_d.set_defaults(func=shuffling_generator)
+    parser_d.set_defaults(func=dinuc_shuffling_generator)
 
 
-def window_shuffling_arg_parsing(subparsers):
+def dinuc_window_shuffling_arg_parsing(subparsers):
     parser_w = subparsers.add_parser(
         'w', help="di-nucleotide shuffling within a sliding window generator")
     parser_w.add_argument(
@@ -159,7 +220,7 @@ def window_shuffling_arg_parsing(subparsers):
     parser_w.add_argument(
         '-n', '--nfold', required=False, type=int, dest="nfold",
         action="store", default=1, help=help_str)
-    parser_w.set_defaults(func=shuffling_window_generator)
+    parser_w.set_defaults(func=dinuc_shuffling_window_generator)
 
 
 def gc_compo_arg_parsing(subparsers):
@@ -226,15 +287,19 @@ def gc_compo_window_arg_parsing(subparsers):
 def arg_parsing():
     descr = '''Background generator with the possibility of using very
     different ways of generating backgrounds lying into two categories:
-      - Creation of new random sequences (generators):
-        - di-nucleotide shuffling using the foreground sequences
-        - di-nucleotide shuffling within a sliding window using foreground
-          sequences
-      - Extraction of sequences from a set of possible background sequences
-      (choosers):
-        - respecting the %GC distribution of the foreground (using %GC bins)
-        - respecting the %GC distribution as in the previous item and also
-        respecting the %GC composition within a sliding window for %GC bin
+        - Creation of new random sequences (generators):
+            - mono-nucleotide shuffling using the foreground sequences
+            - mono-nucleotide shuffling within a sliding window using
+                foreground sequences
+            - di-nucleotide shuffling using the foreground sequences
+            - di-nucleotide shuffling within a sliding window using foreground
+                sequences
+        - Extraction of sequences from a set of possible background sequences
+          (choosers):
+            - respecting the %GC distribution of the foreground (using %GC
+              bins)
+            - respecting the %GC distribution as in the previous item and also
+            respecting the %GC composition within a sliding window for %GC bin
     '''
     parser = argparse.ArgumentParser(
         description=descr,
@@ -242,12 +307,14 @@ def arg_parsing():
     subparsers = parser.add_subparsers(help="Choice of the generator/chooser",
                                        title="Subcommands",
                                        description="Valid subcommands")
-    shuffling_arg_parsing(subparsers)
-    window_shuffling_arg_parsing(subparsers)
+    mononuc_shuffling_arg_parsing(subparsers)
+    mononuc_window_shuffling_arg_parsing(subparsers)
+    dinuc_shuffling_arg_parsing(subparsers)
+    dinuc_window_shuffling_arg_parsing(subparsers)
     gc_compo_arg_parsing(subparsers)
     gc_compo_window_arg_parsing(subparsers)
-    args = parser.parse_args()
-    return args
+    argu = parser.parse_args()
+    return argu
 
 
 def main():
